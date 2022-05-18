6fce78201a4ee144c42a2f0d6c662dcdd8a919b4
amalsyahmi-certifai <76939137+amalsyahmi-certifai@users.noreply.github.com>
2022-05-18 09:49:21 +0800

    react-router simple setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <h1>Let's build sembang</h1>
+      <Router>
+        <Routes>
+          <Route path="/" exact element={<h1>This is the home page</h1>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
