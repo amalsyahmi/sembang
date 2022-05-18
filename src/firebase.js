3ac640038ea857901b6be26785383e92107056b3
amalsyahmi-certifai <76939137+amalsyahmi-certifai@users.noreply.github.com>
2022-05-18 09:39:11 +0800

    init setup and replace npm to yarn

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAOCtduTbsusLcKYNQgkC1nTRBCoyP1xVI",
+  authDomain: "sembang-fb.firebaseapp.com",
+  projectId: "sembang-fb",
+  storageBucket: "sembang-fb.appspot.com",
+  messagingSenderId: "902347371698",
+  appId: "1:902347371698:web:8e84afdb4df16fc68905ab",
+};
